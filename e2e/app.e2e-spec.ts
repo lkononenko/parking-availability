b296a2d166aa040afd6b5f198d37483a7482c962
angular-cli <angular-cli@angular.io>
2017-01-21 14:43:50 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ParkingAvailabilityPage } from './app.po';
+
+describe('parking-availability App', function() {
+  let page: ParkingAvailabilityPage;
+
+  beforeEach(() => {
+    page = new ParkingAvailabilityPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
